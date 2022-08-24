4fce16a8a63cfa2332134c7b9364a6272547b067
Arthur Zhang <60437435+yunchengzhang1@users.noreply.github.com>
2022-08-23 20:22:51 -0500

    testing

--- Digits/ViewController.swift
+++ Digits/ViewController.swift
@@ -16,8 +16,14 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var topCenterLabel: UILabel!
     
+    @IBOutlet weak var botCenterLabel: UILabel!
+    @IBOutlet weak var helloButton: UIButton!
+    weak var counter 
+    
     @IBAction func helloWorldTapped(_ sender: Any) {
         self.topCenterLabel.text = "Hello MSL World!"
+        self.botCenterLabel.text = "hello world"
+        self.helloButton.setTitle("button clicked", for: .normal)
     }
 }
 
